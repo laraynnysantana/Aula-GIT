62e2a9a57fd3bfb414be5686e2d5af5eb7842266
laraynnysantana <dalyllalaraynny@gmail.com>
2026-03-18 16:29:13 -0300

    feat(EX03): pontuação.

--- src/EX03.java
+++ src/EX03.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class EX03 {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        IO.println("Digite seu NickName");
+        String NickName;
+        NickName = sc.next();
+        IO.println("Digite seu Jogo Favorito");
+        String JogoFavorito;
+        JogoFavorito = sc.next();
+        IO.println("Digite sua Pontuação mais Recente");
+        double pontuacão = sc.nextDouble();
+        IO.println(" Informações do Perfil");
+        IO.println("NickName: " + NickName);
+        IO.println("Jogo Favorito: " + JogoFavorito);
+        IO.println("Pontuação: " + pontuacão );
+    }
 }
