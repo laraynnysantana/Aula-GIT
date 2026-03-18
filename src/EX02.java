d32a88f42f6a2d5e2075ee00e140091fa5ed0c95
laraynnysantana <dalyllalaraynny@gmail.com>
2026-03-18 16:13:55 -0300

    feat(EX02): pedido.

--- src/EX02.java
+++ src/EX02.java
@@ -3,6 +3,17 @@ import java.util.Scanner;
 public class EX02 {
     static void main() {
         Scanner sc = new Scanner(System.in);
-
+        IO.println("Digite seu Nome");
+        String nome;
+        nome = sc.next();
+        IO.println(" Digite o prato desejado");
+        String prato;
+        prato = sc.next();
+        IO.println(" Digite o Valor");
+        double valor = sc.nextDouble();
+        IO.println(" Resumo do Pedido");
+        IO.println("Nome:" + nome);
+        IO.println("Prato:" + prato);
+        IO.println("Valor:" + valor );
     }
 }
