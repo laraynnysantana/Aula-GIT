6b5f93fd0672daa7dcdb772f30a07c47133352e3
LARAYNNY DALYLLA SANTANA RIBEIRO <laraynny.ribeiro@senaisp.edu.br>
2026-03-20 16:20:41 -0300

    feat(EX09):frete gratis.

--- src/EX09.java
+++ src/EX09.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
+
 public class EX09{
 }
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        IO.println(" frete gratis a cima de 100 reais");
+        IO.println(" digite o valor da sua compra");
+        double compra;
+        compra = sc.nextDouble();
+        if (compra >= 100) {
+            IO.println(" parabens voce tem direito a frete gratis");
+        } else if (compra <= 100) {
+            IO.println("voce não atingiu o valor necessario para conseguir o frete gratis e falta ");
+            IO.println(compra - 100);
+        }
+    }
+
+
